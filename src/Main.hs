5078414fe6979b4c54e678e312c1f3185bd32313
Miles Frankel <miles.frankel@gmail.com>
2015-09-29 17:38:21 -0400

    fixed api key env

--- src/Main.hs
+++ src/Main.hs
@@ -6,75 +6,17 @@
 {-# LANGUAGE TypeOperators     #-}
 
 module Main where
-
-
 import           Control.Monad.Trans.Either
 import           Data.Aeson
 import           Data.Aeson.TH
 import           GHC.Generics
 import           Servant
 import           Servant.Client
+import           System.Environment
+
+import           Types
 
 type GeoLocAPI = "geolocation" :> "v1" :> "geolocate" :> QueryParam "key" String :> ReqBody '[JSON] GeoReq :> Post '[JSON] GeoResp
-
-data RadioType = LTE
-               | GSM
-               | CDMA
-               | WCDMA
-               deriving (Eq, Ord, Generic)
-instance ToJSON RadioType
-instance Show RadioType where
-  show LTE = "lte"
-  show GSM = "gsm"
-  show CDMA = "cdma"
-  show WCDMA = "wcdma"
-
-
-newtype BoolLower = BoolLower Bool  deriving (Eq, Ord, Generic)
-instance ToJSON BoolLower
-instance Show BoolLower where
-  show (BoolLower True) = "true"
-  show (BoolLower False) = "false"
-
-data GeoReq = GeoReq { homeMobileCountryCode :: Maybe String
-                     , homeMobileNetworkCode :: Maybe String
-                     , radioType             :: Maybe RadioType
-                     , carrier               :: Maybe String
-                     , considerIp            :: Maybe BoolLower
-                     , cellTowers            :: Maybe [Tower]
-                     , wifiAccessPoints      :: [WAP]
-                     } deriving (Show, Eq, Ord, Generic)
--- instance ToJSON GeoReq
-instance ToJSON GeoReq where
-      toJSON = genericToJSON $ defaultOptions { omitNothingFields = True }
-
-data Tower = Tower { cellId            :: Maybe String
-                   , locationAreaCode  :: String
-                   , mobileCountryCode :: String
-                   , mobileNetworkCode :: String
-                   } deriving (Show, Eq, Ord, Generic)
-instance ToJSON Tower where
-      toJSON = genericToJSON $ defaultOptions { omitNothingFields = True }
-
-
-data WAP = WAP { macAddress         :: String
-               , signalStrength     :: Maybe Float
-               , age                :: Maybe Int
-               , channel            :: Maybe Int
-               , signalToNoiseRatio :: Maybe Float
-               } deriving (Show, Eq, Ord, Generic)
-instance ToJSON WAP where
-      toJSON = genericToJSON $ defaultOptions { omitNothingFields = True }
-
-
-data GeoResp = GeoResp { location :: Location
-                       , accuracy :: Float
-                       } deriving (Show, Eq, Ord, Generic)
-data Location = Location { lat :: Float
-                         , lng :: Float
-                         } deriving (Show, Eq, Ord, Generic)
-instance FromJSON GeoResp
-instance FromJSON Location
 
 api :: Proxy GeoLocAPI
 api = Proxy
@@ -83,11 +25,11 @@ geolocate :: Maybe String -> GeoReq -> EitherT ServantError IO GeoResp
 geolocate = client api (BaseUrl Https "www.googleapis.com" 443)
 
 queries :: String -> EitherT ServantError IO (GeoResp)
-queries = geolocate (Just key) req
+queries key = geolocate (Just key) req
 
 main :: IO ()
 main = do key <- getEnv "API_KEY"
-          r <- runEitherT $ queries $ fromJust key
+          r <- runEitherT $ queries key
           case r of
            Left err -> print err
            Right resp -> print resp
